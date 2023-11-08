79425b98f67538c9968fa86038efd41c45cbadb4
dakkusu988 <bydaxplayyt@gmail.com>
2023-11-08 10:35:46 +0100

    Completando con demas archivos y contenido 1

--- task/views.py
+++ task/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForms
 
 def task_list(request):
   tasks = Task.objects.all()
-  return render(request, 'task/task_list.html', {})
+
+  if request.method == 'POST':
+    form=TaskForms(request.POST)
+
+    if form.is_valid():
+      form.save()
+      return redirect('task_list')
+    
+  else:
+    form= TaskForms()
+  return render(request, 'task/task_list.html', {'task':tasks, form})
